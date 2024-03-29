f06173d9214234d0f5f39e9c4b6a401ba7382e57
RAJA LAKSHMI R <127002476+RajalakshmiR24@users.noreply.github.com>
2024-03-29 17:40:56 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom/client';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-reportWebVitals(sendToVercelAnalytics);
+root.render(<App />);
